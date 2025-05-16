8345014abd026ff7c041e69f591ab3fdc5272913
Tony Mcrae <tony.mcrae@Outlook.com>
2025-05-15 22:03:47 -0400

    pls commit to main

--- testcommit.ps1
+++ testcommit.ps1
@@ -3,3 +3,7 @@ write-host 'hi again'
 write-host 'test after modification'
 
 write-host 'I did this in a test branch!'
+
+write-host 'comitting to main'
+
+write-host 'why isnt this letting me commit'
